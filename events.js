e7de5be9b0ea16a06d134fa632fee8237e04a54f
Liz Starin <liz.starin@gmail.com>
2018-12-01 22:10:29 -0500

    places icons on map corresponding to events

--- events.js
+++ events.js
@@ -4,16 +4,56 @@ window.onload = function(e) {
 	var map = new mapboxgl.Map({
 		container: 'map',
 		style: 'mapbox://styles/mapbox/streets-v10',
-		center: [-73.95, 40.65],
-		zoom: 10
+		center: [-95, 39],
+		zoom: 3.5
 	});
+
+	var buildGeoJson = function(json) {
+		var events = json.data;
+		var located = events.filter(e => e.location != null);
+
+		var features = []
+		located.forEach(function(e) {
+			var feature = {
+				type: 'Feature',
+				geometry: {
+					type: 'Point',
+					coordinates: [e.location.location.longitude, e.location.location.latitude]
+				},
+				properties: {
+					title: e.title,
+					description: e.description
+				}
+			};
+
+			features.push(feature);
+		});
+
+		return {
+			type: 'FeatureCollection',
+			features: features
+		};
+	};
+
+	var addMarkers = function(geojson) {
+		geojson.features.forEach(function(marker) {
+
+		  var el = document.createElement('div');
+		  el.className = 'marker';
+
+		  new mapboxgl.Marker(el)
+		  .setLngLat(marker.geometry.coordinates)
+		  .addTo(map);
+		});	
+	};
 
 	fetch('https://events.mobilizeamerica.io/api/v1/events')
 	  .then(function(response) {
 	    return response.json();
 	  })
 	  .then(function(eventsJson) {
-	    console.log(JSON.stringify(eventsJson));
+	    var geojson = buildGeoJson(eventsJson);
+	    addMarkers(geojson);
 	  });
-	
+
 }
