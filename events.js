e408792850ea44eb1d938b5832cecc9f4609ba8f
Liz Starin <liz.starin@gmail.com>
2018-12-02 21:29:27 -0500

    little bit of code cleanup, empty test methods, error handling for API call

--- events.js
+++ events.js
@@ -1,72 +1,91 @@
-window.onload = function(e) {
-	mapboxgl.accessToken = 'pk.eyJ1IjoibGl6c3RhcmluIiwiYSI6ImNqcDRjbjA0eDBmenIzanA3Znd5MnBvd3gifQ.feWymrUSbwc9izQhA-HqpQ'; // Public token
+var eventsList = function () {
+	window.onload = function(e) {
+		mapboxgl.accessToken = 'pk.eyJ1IjoibGl6c3RhcmluIiwiYSI6ImNqcDRjbjA0eDBmenIzanA3Znd5MnBvd3gifQ.feWymrUSbwc9izQhA-HqpQ'; // Public token
 
-	var map = new mapboxgl.Map({
-		container: 'map',
-		style: 'mapbox://styles/mapbox/streets-v10',
-		center: [-95, 39],
-		zoom: 3.5
-	});
-
-	var buildGeoJson = function(events) {
-		var located = events.filter(e => e.location != null);
-
-		var features = []
-		located.forEach(function(e) {			// I'm not sure why Array.map didn't work, but it didn't!
-			var feature = {
-				type: 'Feature',
-				geometry: {
-					type: 'Point',
-					coordinates: [e.location.location.longitude, e.location.location.latitude]
-				},
-				properties: {
-					title: e.title,
-					description: e.description,
-					url: e.browser_url
-				}
-			};
-
-			features.push(feature);
-		});
-
-		return {
-			type: 'FeatureCollection',
-			features: features
+		var drawMap = function() {
+			return new mapboxgl.Map({
+				container: 'map',
+				style: 'mapbox://styles/mapbox/streets-v10',
+				center: [-95, 39],
+				zoom: 3.5
+			});
 		};
-	};
 
-	var addMarkers = function(geojson) {
-		geojson.features.forEach(function(marker) {
+		var buildGeoJson = function(events) {
+			var located = events.filter(e => e.location != null);
 
-			var el = document.createElement('div');
-		  	el.className = 'marker';
+			var features = []
+			located.forEach(function(e) {			// I'm not sure why Array.map didn't work, but it didn't!
+				var feature = {
+					type: 'Feature',
+					geometry: {
+						type: 'Point',
+						coordinates: [e.location.location.longitude, e.location.location.latitude]
+					},
+					properties: {
+						title: e.title,
+						description: e.description,
+						url: e.browser_url,
+						id: e.id
+					}
+				};
 
-		  	new mapboxgl.Marker(el)
-		  	.setLngLat(marker.geometry.coordinates)
-		  	.setPopup(new mapboxgl.Popup({ offset: 25 })
-  			.setHTML('<a href="' + marker.properties.url + '"><h3>' + marker.properties.title + '</h3></a><p>' + marker.properties.description + '</p>'))
-		  	.addTo(map);
-		});	
-	};
+				features.push(feature);
+			});
 
-	var addList = function(events) {
-		events.forEach(function(e) {
-			var el = document.createElement('div');
-			var content = document.createTextNode(e.title);
-			el.appendChild(content);
-			document.getElementById('list').appendChild(el);
-		});
-	};
+			return {
+				type: 'FeatureCollection',
+				features: features
+			};
+		};
 
-	fetch('https://events.mobilizeamerica.io/api/v1/events')
-		.then(function(response) {
-	    	return response.json();
-	  	})
-	  	.then(function(eventsJson) {
-	  		var events = eventsJson.data
-	    	var geojson = buildGeoJson(events);
-	    	addMarkers(geojson);
-	    	addList(events);
-	  	});
+		var addMarkers = function(geojson) {
+			geojson.features.forEach(function(marker) {
 
-}
+				var el = document.createElement('div');
+			  	el.className = 'marker';
+			  	el.dataset.id = marker.properties.id;
+
+			  	new mapboxgl.Marker(el)
+			  		.setLngLat(marker.geometry.coordinates)
+			  		.setPopup(new mapboxgl.Popup({ offset: 25 })
+	  				.setHTML('<a href="' + marker.properties.url + '"><h3>' + marker.properties.title + '</h3></a><p>' + marker.properties.description + '</p>'))
+			  		.addTo(map);
+			});	
+		};
+
+		var addList = function(events) {
+			events.forEach(function(e) {
+				var el = document.createElement('div');
+				var content = document.createTextNode(e.title);
+				el.className = 'item';
+				el.appendChild(content);
+				el.dataset.id = e.id;
+				document.getElementById('list').appendChild(el);
+			});
+		};
+
+		var getData = function() {
+			fetch('https://events.mobilizeamerica.io/api/v1/events')
+				.then(function(response) {
+					if (response.ok) {
+			    		return response.json();
+					} else {
+						response.redirect('error.html');
+					}
+			  	})
+			  	.then(function(eventsJson) {
+			  		var events = eventsJson.data
+			    	var geojson = buildGeoJson(events);
+			    	addMarkers(geojson);
+			    	addList(events);
+			  	}).catch(function(error) {
+			  		console.log('fetch error: ', error.message);
+			  		window.location = 'error.html';
+			  	});
+		};
+
+		var map = drawMap();
+		getData();
+	}
+} ();
--- jasmine-standalone-3.3.0/spec/events.spec.js
+++ jasmine-standalone-3.3.0/spec/events.spec.js
@@ -1,5 +1,21 @@
 describe('Events', function() {
-	it('downloads stuff from api', function() {
+	it('downloads data from MobilizeAmerica API', function() {
+
+	});
+
+	it('handles network errors', function() {
+
+	});
+
+	it('creates a map', function() {
+
+	});
+
+	it('creates and appends map markers', function() {
+
+	});
+
+	it('creates and appends an events list', function() {
 
 	});
 });
